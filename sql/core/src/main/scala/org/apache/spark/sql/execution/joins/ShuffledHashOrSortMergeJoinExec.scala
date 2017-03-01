d203de79b48f05cc94e732a70faa468d421d4360
Tomer Kaftan <tomer.kaftan@gmail.com>
2017-03-01 09:42:17 -0800

    WIP TPCDS

---
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/joins/ShuffledHashOrSortMergeJoinExec.scala
@@ -0,0 +1,317 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.spark.sql.execution.joins
+
+import org.apache.spark.TaskContext
+import org.apache.spark.bandit.policies.EpsilonGreedyPolicyParams
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.catalyst.InternalRow
+import org.apache.spark.sql.catalyst.expressions.{Attribute, Expression, GenericInternalRow, JoinedRow, Projection, UnsafeProjection}
+import org.apache.spark.sql.catalyst.plans._
+import org.apache.spark.sql.catalyst.plans.physical._
+import org.apache.spark.sql.execution.metric.{SQLMetric, SQLMetrics}
+import org.apache.spark.sql.execution.{BinaryExecNode, RowIterator, SparkPlan}
+import org.apache.spark.util.collection.{BitSet, ExternalSorter}
+
+import scala.collection.mutable.ArrayBuffer
+
+/**
+ * Performs a join of two child relations by first shuffling the data using the join keys,
+ * Then selecting either a sort-merge join or a hash join for each partition
+ */
+case class ShuffledHashOrSortMergeJoinExec(
+    leftKeys: Seq[Expression],
+    rightKeys: Seq[Expression],
+    joinType: JoinType,
+    buildSide: BuildSide,
+    condition: Option[Expression],
+    left: SparkPlan,
+    right: SparkPlan)
+  extends BinaryExecNode with HashJoin {
+
+  //val sc = left.sqlContext.sparkContext
+  def joinByHash(in: (Seq[InternalRow], Seq[InternalRow], SQLMetric,
+    Seq[Attribute], Seq[Attribute])): Iterator[InternalRow] = {
+    val hashed = buildHashedRelation(in._2.iterator)
+    join(in._1.iterator, hashed, in._3)
+  }
+
+  def joinBySort(in: (Iterator[InternalRow], Iterator[InternalRow], SQLMetric,
+    Seq[Attribute], Seq[Attribute])): Iterator[InternalRow] = {
+    val (leftOutput, rightOutput) = (in._4, in._5)
+    val boundCondition: (InternalRow) => Boolean = {
+      condition.map { cond =>
+        newPredicate(cond, leftOutput ++ rightOutput).eval _
+      }.getOrElse {
+        (r: InternalRow) => true
+      }
+    }
+
+    // An ordering that can be used to compare keys from both sides.
+    val keyOrdering = newNaturalAscendingOrdering(leftKeys.map(_.dataType))
+    val resultProj: InternalRow => InternalRow = UnsafeProjection.create(output, output)
+
+    val leftSorter = new ExternalSorter[InternalRow, Null, InternalRow](
+      TaskContext.get(), ordering = Some(keyOrdering))
+    leftSorter.insertAll(in._1.map(r => (r.copy(), null)))
+    val leftIter = leftSorter.iterator.map(_._1)
+
+    val rightSorter = new ExternalSorter[InternalRow, Null, InternalRow](
+      TaskContext.get(), ordering = Some(keyOrdering))
+    rightSorter.insertAll(in._2.map(r => (r.copy(), null)))
+    val rightIter = rightSorter.iterator.map(_._1)
+    val numOutputRows = in._3
+
+    joinType match {
+      case _: InnerLike =>
+        new RowIterator {
+          private[this] var currentLeftRow: InternalRow = _
+          private[this] var currentRightMatches: ArrayBuffer[InternalRow] = _
+          private[this] var currentMatchIdx: Int = -1
+          private[this] val smjScanner = new SortMergeJoinScanner(
+            UnsafeProjection.create(leftKeys, left.output),
+            UnsafeProjection.create(rightKeys, right.output),
+            keyOrdering,
+            RowIterator.fromScala(leftIter),
+            RowIterator.fromScala(rightIter)
+          )
+          private[this] val joinRow = new JoinedRow
+
+          if (smjScanner.findNextInnerJoinRows()) {
+            currentRightMatches = smjScanner.getBufferedMatches
+            currentLeftRow = smjScanner.getStreamedRow
+            currentMatchIdx = 0
+          }
+
+          override def advanceNext(): Boolean = {
+            while (currentMatchIdx >= 0) {
+              if (currentMatchIdx == currentRightMatches.length) {
+                if (smjScanner.findNextInnerJoinRows()) {
+                  currentRightMatches = smjScanner.getBufferedMatches
+                  currentLeftRow = smjScanner.getStreamedRow
+                  currentMatchIdx = 0
+                } else {
+                  currentRightMatches = null
+                  currentLeftRow = null
+                  currentMatchIdx = -1
+                  return false
+                }
+              }
+              joinRow(currentLeftRow, currentRightMatches(currentMatchIdx))
+              currentMatchIdx += 1
+              if (boundCondition(joinRow)) {
+                numOutputRows += 1
+                return true
+              }
+            }
+            false
+          }
+
+          override def getRow: InternalRow = resultProj(joinRow)
+        }.toScala
+
+      case LeftOuter =>
+        val smjScanner = new SortMergeJoinScanner(
+          streamedKeyGenerator = UnsafeProjection.create(leftKeys, leftOutput),
+          bufferedKeyGenerator = UnsafeProjection.create(rightKeys, rightOutput),
+          keyOrdering,
+          streamedIter = RowIterator.fromScala(leftIter),
+          bufferedIter = RowIterator.fromScala(rightIter)
+        )
+        val rightNullRow = new GenericInternalRow(right.output.length)
+        new LeftOuterIterator(
+          smjScanner, rightNullRow, boundCondition, resultProj, numOutputRows).toScala
+
+      case RightOuter =>
+        val smjScanner = new SortMergeJoinScanner(
+          streamedKeyGenerator = UnsafeProjection.create(rightKeys, rightOutput),
+          bufferedKeyGenerator = UnsafeProjection.create(leftKeys, leftOutput),
+          keyOrdering,
+          streamedIter = RowIterator.fromScala(rightIter),
+          bufferedIter = RowIterator.fromScala(leftIter)
+        )
+        val leftNullRow = new GenericInternalRow(leftOutput.length)
+        new RightOuterIterator(
+          smjScanner, leftNullRow, boundCondition, resultProj, numOutputRows).toScala
+
+      case FullOuter =>
+        val leftNullRow = new GenericInternalRow(leftOutput.length)
+        val rightNullRow = new GenericInternalRow(rightOutput.length)
+        val smjScanner = new SortMergeFullOuterJoinScanner(
+          leftKeyGenerator = UnsafeProjection.create(leftKeys, leftOutput),
+          rightKeyGenerator = UnsafeProjection.create(rightKeys, rightOutput),
+          keyOrdering,
+          leftIter = RowIterator.fromScala(leftIter),
+          rightIter = RowIterator.fromScala(rightIter),
+          boundCondition,
+          leftNullRow,
+          rightNullRow)
+
+        new FullOuterIterator(
+          smjScanner,
+          resultProj,
+          numOutputRows).toScala
+
+      case LeftSemi =>
+        new RowIterator {
+          private[this] var currentLeftRow: InternalRow = _
+          private[this] val smjScanner = new SortMergeJoinScanner(
+            UnsafeProjection.create(leftKeys, leftOutput),
+            UnsafeProjection.create(rightKeys, rightOutput),
+            keyOrdering,
+            RowIterator.fromScala(leftIter),
+            RowIterator.fromScala(rightIter)
+          )
+          private[this] val joinRow = new JoinedRow
+
+          override def advanceNext(): Boolean = {
+            while (smjScanner.findNextInnerJoinRows()) {
+              val currentRightMatches = smjScanner.getBufferedMatches
+              currentLeftRow = smjScanner.getStreamedRow
+              var i = 0
+              while (i < currentRightMatches.length) {
+                joinRow(currentLeftRow, currentRightMatches(i))
+                if (boundCondition(joinRow)) {
+                  numOutputRows += 1
+                  return true
+                }
+                i += 1
+              }
+            }
+            false
+          }
+
+          override def getRow: InternalRow = currentLeftRow
+        }.toScala
+
+      case LeftAnti =>
+        new RowIterator {
+          private[this] var currentLeftRow: InternalRow = _
+          private[this] val smjScanner = new SortMergeJoinScanner(
+            UnsafeProjection.create(leftKeys, leftOutput),
+            UnsafeProjection.create(rightKeys, rightOutput),
+            keyOrdering,
+            RowIterator.fromScala(leftIter),
+            RowIterator.fromScala(rightIter)
+          )
+          private[this] val joinRow = new JoinedRow
+
+          override def advanceNext(): Boolean = {
+            while (smjScanner.findNextOuterJoinRows()) {
+              currentLeftRow = smjScanner.getStreamedRow
+              val currentRightMatches = smjScanner.getBufferedMatches
+              if (currentRightMatches == null) {
+                return true
+              }
+              var i = 0
+              var found = false
+              while (!found && i < currentRightMatches.length) {
+                joinRow(currentLeftRow, currentRightMatches(i))
+                if (boundCondition(joinRow)) {
+                  found = true
+                }
+                i += 1
+              }
+              if (!found) {
+                numOutputRows += 1
+                return true
+              }
+            }
+            false
+          }
+
+          override def getRow: InternalRow = currentLeftRow
+        }.toScala
+
+      case j: ExistenceJoin =>
+        new RowIterator {
+          private[this] var currentLeftRow: InternalRow = _
+          private[this] val result: InternalRow = new GenericInternalRow(Array[Any](null))
+          private[this] val smjScanner = new SortMergeJoinScanner(
+            UnsafeProjection.create(leftKeys, leftOutput),
+            UnsafeProjection.create(rightKeys, rightOutput),
+            keyOrdering,
+            RowIterator.fromScala(leftIter),
+            RowIterator.fromScala(rightIter)
+          )
+          private[this] val joinRow = new JoinedRow
+
+          override def advanceNext(): Boolean = {
+            while (smjScanner.findNextOuterJoinRows()) {
+              currentLeftRow = smjScanner.getStreamedRow
+              val currentRightMatches = smjScanner.getBufferedMatches
+              var found = false
+              if (currentRightMatches != null) {
+                var i = 0
+                while (!found && i < currentRightMatches.length) {
+                  joinRow(currentLeftRow, currentRightMatches(i))
+                  if (boundCondition(joinRow)) {
+                    found = true
+                  }
+                  i += 1
+                }
+              }
+              result.setBoolean(0, found)
+              numOutputRows += 1
+              return true
+            }
+            false
+          }
+
+          override def getRow: InternalRow = resultProj(joinRow(currentLeftRow, result))
+        }.toScala
+
+      case x =>
+        throw new IllegalArgumentException(
+          s"SortMergeJoin should not take $x as the JoinType")
+    }
+  }
+
+  //val bandit = sc.bandit(Seq(x => joinBySort(x)), EpsilonGreedyPolicyParams())
+
+  override lazy val metrics = Map(
+    "numOutputRows" -> SQLMetrics.createMetric(sparkContext, "number of output rows"),
+    "buildDataSize" -> SQLMetrics.createSizeMetric(sparkContext, "data size of build side"),
+    "buildTime" -> SQLMetrics.createTimingMetric(sparkContext, "time to build hash map"))
+
+  override def requiredChildDistribution: Seq[Distribution] =
+    ClusteredDistribution(leftKeys) :: ClusteredDistribution(rightKeys) :: Nil
+
+  private def buildHashedRelation(iter: Iterator[InternalRow]): HashedRelation = {
+    val buildDataSize = longMetric("buildDataSize")
+    val buildTime = longMetric("buildTime")
+    val start = System.nanoTime()
+    val context = TaskContext.get()
+    val relation = HashedRelation(iter, buildKeys, taskMemoryManager = context.taskMemoryManager())
+    buildTime += (System.nanoTime() - start) / 1000000
+    buildDataSize += relation.estimatedSize
+    // This relation is usually used until the end of task.
+    context.addTaskCompletionListener(_ => relation.close())
+    relation
+  }
+
+  protected override def doExecute(): RDD[InternalRow] = {
+    val numOutputRows = longMetric("numOutputRows")
+    streamedPlan.execute().zipPartitions(buildPlan.execute()) { (streamIter, buildIter) =>
+      val streamIterSeq = streamIter
+      val buildIterSeq = buildIter
+      joinBySort((streamIterSeq, buildIterSeq, numOutputRows, left.output, right.output))
+    }
+  }
+
+}
--- sql/core/src/test/scala/org/apache/spark/sql/execution/benchmark/TPCDSQueryBenchmark.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/execution/benchmark/TPCDSQueryBenchmark.scala
@@ -36,12 +36,13 @@ import org.apache.spark.util.Benchmark
 object TPCDSQueryBenchmark {
   val conf =
     new SparkConf()
-      .setMaster("local[1]")
+      .setMaster("local[4]")
       .setAppName("test-sql-context")
       .set("spark.sql.parquet.compression.codec", "snappy")
-      .set("spark.sql.shuffle.partitions", "4")
+      .set("spark.sql.shuffle.partitions", "16")
       .set("spark.driver.memory", "3g")
       .set("spark.executor.memory", "3g")
+        //.set("spark.sql.codegen.wholeStage", "false")
       .set("spark.sql.autoBroadcastJoinThreshold", (20 * 1024 * 1024).toString)
 
   val spark = SparkSession.builder.config(conf).getOrCreate()
@@ -100,7 +101,13 @@ object TPCDSQueryBenchmark {
   def main(args: Array[String]): Unit = {
 
     // List of all TPC-DS queries
-    val tpcdsQueries = Seq(
+    val tpcdsQueries = Seq("q72")// Seq("q49", "q72", "q75", "q78", "q80", "q93")
+    // "q77" gives error
+    // "q5" and "q40" are only broadcast joins w/ the current scale factor of 5 & 16 partitions
+    // "q51" and "q97" don't support hash joins because not a hashable relation
+    // All the other queries don't support joins
+
+    /*Seq(
       "q1", "q2", "q3", "q4", "q5", "q6", "q7", "q8", "q9", "q10", "q11",
       "q12", "q13", "q14a", "q14b", "q15", "q16", "q17", "q18", "q19", "q20",
       "q21", "q22", "q23a", "q23b", "q24a", "q24b", "q25", "q26", "q27", "q28", "q29", "q30",
@@ -110,14 +117,50 @@ object TPCDSQueryBenchmark {
       "q61", "q62", "q63", "q64", "q65", "q66", "q67", "q68", "q69", "q70",
       "q71", "q72", "q73", "q74", "q75", "q76", "q77", "q78", "q79", "q80",
       "q81", "q82", "q83", "q84", "q85", "q86", "q87", "q88", "q89", "q90",
-      "q91", "q92", "q93", "q94", "q95", "q96", "q97", "q98", "q99")
+      "q91", "q92", "q93", "q94", "q95", "q96", "q97", "q98", "q99")*/
 
     // In order to run this benchmark, please follow the instructions at
     // https://github.com/databricks/spark-sql-perf/blob/master/README.md to generate the TPCDS data
     // locally (preferably with a scale factor of 5 for benchmarking). Thereafter, the value of
     // dataLocation below needs to be set to the location where the generated data is stored.
-    val dataLocation = ""
+    val dataLocation = "/Users/tomerk11/Desktop/tpcds-data"
 
     tpcdsAll(dataLocation, queries = tpcdsQueries)
+  }
+}
+
+object TPCDSDataGen {
+  val conf =
+    new SparkConf()
+      .setMaster("local[4]")
+      .setAppName("tpcds-data-gen")
+      .set("spark.sql.parquet.compression.codec", "snappy")
+      .set("spark.driver.memory", "3g")
+      .set("spark.executor.memory", "3g")
+
+  val spark = SparkSession.builder.config(conf).getOrCreate()
+
+  def main(args: Array[String]): Unit = {
+    val dsdgenDir = "/Users/tomerk11/Development/tpcds-kit/tools"
+    val scaleFactor = 5
+    import org.apache.spark.sql.execution.benchmark.tpcds.Tables
+    // Tables in TPC-DS benchmark used by experiments.
+    // dsdgenDir is the location of dsdgen tool installed in your machines.
+    val tables = new Tables(spark.sqlContext, dsdgenDir, scaleFactor)
+    // Generate data.
+    tables.genData("/Users/tomerk11/Desktop/tpcds-data",
+      "parquet",
+      true,
+      true,
+      true,
+      false,
+      false,
+      numPartitions = 16)
+    // Create metastore tables in a specified database for your data.
+    // Once tables are created, the current database will be switched to the specified database.
+    //tables.createExternalTables(location, format, databaseName, overwrite)
+    // Or, if you want to create temporary tables
+    //tables.createTemporaryTables(location, format)
+    // Setup TPC-DS experiment
   }
 }
