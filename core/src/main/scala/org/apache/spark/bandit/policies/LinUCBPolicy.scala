93e997f3c9041a7463cfcf278836a0f943c40d94
Tomer Kaftan <tomer.kaftan@gmail.com>
2017-04-13 11:38:11 -0700

    Bug fix in LinUCB

--- core/src/main/scala/org/apache/spark/bandit/policies/LinUCBPolicy.scala
+++ core/src/main/scala/org/apache/spark/bandit/policies/LinUCBPolicy.scala
@@ -47,10 +47,10 @@ private[spark] class LinUCBPolicy(numArms: Int, numFeatures: Int, alpha: Double)
     // TODO: Should be able to optimize code by only computing coefficientEstimate after
     // updates. Would require an update to ContextualBanditPolicy to apply optimization
     // to all contextual bandits.
-    if (armRewardsStats.totalWeights >= 2) {
+    if (armRewardsStats.totalWeights > 2) {
       val coefficientEstimate = armFeaturesAcc \ armRewardsAcc
-      coefficientEstimate.t * features + alpha * armRewardsStats.variance * sqrt(
-        features.t * (armFeaturesAcc \ features)
+      coefficientEstimate.t * features + alpha * sqrt(
+        features.t * armRewardsStats.variance * (armFeaturesAcc \ features)
       )
     } else {
       Double.PositiveInfinity
